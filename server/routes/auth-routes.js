418b58cdc8ffa75176e5769b9912ef38856e304c
Tai Huynh <anhtaih89@gmail.com>
2016-06-20 18:06:32 -0700

    Refactor routes out to individual files, for protected vs unprotected routes

---
+++ server/routes/auth-routes.js
@@ -0,0 +1,16 @@
+const ctrlAuth = require('../controllers/auth-controller');
+
+module.exports = (app) => {
+  app.post('/api/login', ctrlAuth.login);
+  app.post('/api/signup', ctrlAuth.signup);
+
+  app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+  });
+
+   // Catch all;
+  app.get('/*', (req, res) => {
+    res.redirect('/');
+  });
+};
