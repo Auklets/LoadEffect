1657fef3fddd84faed8c7a82dab6c01a6949339a
Tai Huynh <anhtaih89@gmail.com>
2016-06-20 18:08:03 -0700

    Rename from client.js to entry.js to indicate where entry point is (for webpack)

---
+++ client/entry.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute } from 'react-router';
+import store, { history } from './redux/store';
+
+import AppContainer from './components/AppContainer.jsx';
+import SignupContainer from './components/Signup/SignupContainer.jsx';
+import LoginContainer from './components/Login/LoginContainer.jsx';
+import HomeContainer from './components/Home/HomeContainer.jsx';
+import MainContainer from './components/Main/MainContainer.jsx';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={AppContainer}>
+        <IndexRoute component={HomeContainer} />
+        <Route path="/login" component={LoginContainer} />
+        <Route path="/main" component={MainContainer} />
+        <Route path="/signup" component={SignupContainer} />
+      </Route>
+    </Router>
+  </Provider>
+);
+
+render(router, document.getElementById('app'));
