be3c52b82139c5892e46bce0a2b99e7a44d6f034
Tai Huynh <anhtaih89@gmail.com>
2016-06-20 18:09:40 -0700

    Refactor middleware code to clean up

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const session = require('express-session');
 const morgan = require('morgan');
+const passport = require('passport');
 
 const config = require('../../webpack.config.js');
 const webpack = require('webpack');
@@ -14,6 +15,9 @@ module.exports = (app, express) => {
   app.use(webpackHotMiddleware(compiler));
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.use(session({ secret: 'fred', resave: false, saveUninitialized: false }));
   app.use(express.static(path.join(`${__dirname}./../../dist`)));
 };
