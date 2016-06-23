8a3bdb0f5326b462d5dd6ac32799a352279c1f0e
Tai Huynh <anhtaih89@gmail.com>
2016-06-22 19:48:36 -0700

    setup

--- tests/testSetup.js
+++ tests/testSetup.js
@@ -19,11 +19,11 @@
  */
 // process.env.NODE_ENV = 'test';
 
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config({ path: './env/development.env' });
-} else if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config({ path: './env/production.env' });
-}
+// if (process.env.NODE_ENV === 'development') {
+//   require('dotenv').config({ path: './env/development.env' });
+// } else if (process.env.NODE_ENV === 'production') {
+//   require('dotenv').config({ path: './env/production.env' });
+// }
 
 // Register babel so that it will transpile ES6 to ES5
 // before our tests run.
