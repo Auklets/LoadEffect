f2d47b6bb2615e4bca0454500b4ac6a341c5c096
Tai Huynh <anhtaih89@gmail.com>
2016-06-22 18:19:55 -0700

    Edit tests

--- tests/server-test/server.spec.js
+++ tests/server-test/server.spec.js
@@ -1,67 +1,27 @@
-/* eslint-disable */
 const expect = require('chai').expect;
-const app = 'http://localhost:8000'//require('../../server/server');
+const app = `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}`;
 const request = require('supertest');
 const User = require('../../server/models/UsersModel.js');
 
-describe('Express Server', function() {
-  describe('Basic HTTP Request:', function() {
-    it('Returns a 302 status code on GET requests /', function(done) {
-      request(app).get('/')
-        .expect(404)
-        .end(function(err) {
-          if (err) {
-            done(err);
-          } else {
-            done();
-          }
-        })
-    });
-
-    xit('Logs in existing users', function(done) {
-      request(app)
-        .post('/api/login')
-        .send({
-          'email': 'Anhtaih@hackreactor.com',
-          'password': 'password-to-be-salted-and-hashed' })
-        .expect(200)
-        .expect(function(res) {
-          expect(res.headers.location).to.equal('/');
-        })
-        .end(done());
-    });
-
-    xit('Users that do not exist are kept on login page', function(done) {
-      request(app)
-        .post('/api/login')
-        .send({
-          'username': 'Bill',
-          'password': 'Haug' })
-        .expect(401)
-        .expect(function(res) {
-          expect(res.headers.location).to.equal('/login'); // currently broken
-        })
-        .end(done());
-    });
-  }); // Account Login
-
-  xdescribe('Priviledged Access:', function() {
-
-    // /*  Authentication  */
-    // // TODO: xit out authentication
-    it('Redirects to home page if a user tries to access a protected route and is not signed in', function(done) {
+xdescribe('Express Server', () => {
+  describe('Basic HTTP Request:', () => {
+    it('should return a 200 status code on GET requests to /', done => {
       request(app)
         .get('/')
-        .expect(302)
-        .expect(function(res) {
-          expect(res.headers.location).to.equal('/');
-        })
-        .end(done());
+        .expect(200)
+        .end(done);
     });
-  }); // 'Privileged Access'
 
-  xdescribe('Account Creation:', function() {
-    it('Signup creates a new user', function(done) {
+    it('should respond with 404 if user inputs invalid url', done => {
+      request(app)
+        .get('/nonexistent')
+        .expect(404)
+        .end(done);
+    });
+  });
+
+  describe('Account Creation:', () => {
+    it('Signup creates a new user', done => {
       request(app)
         .post('/api/signup')
         .send({
@@ -69,29 +29,13 @@ describe('Express Server', function() {
           name: 'Felix Ramsey',
           password: 'loadtest' })
         .expect(200)
-        .expect(function() {
+        .expect(() => {
           User.findOne({ 'email': 'me@auklets.com' })
-            .exec(function(err, user) {
+            .exec((err, user) => {
               expect(user.name).to.equal('Felix Ramsey');
             });
         })
-        .end(done());
+        .end(done);
     });
-
-    it('Successful signup logs in a new user', function(done) {
-      request(app)
-        .post('/api/signup')
-        .send({
-          'email': 'Chris@me.com',
-          'password': 'dontHaug' })
-        .expect(200)
-        .expect(function(res) {
-          expect(res.headers.location).to.equal('/');
-          request(app)
-            .get('/api/logout')
-            .expect(302);
-        })
-        .end(done());
-    });
-  }); // 'Account Creation'
+  });
 });
