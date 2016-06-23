a6a8f12c759a653297fbdcd7eb96a2f25a6591b6
Tai Huynh <anhtaih89@gmail.com>
2016-06-23 16:18:30 -0700

    Change test files to work locally and integration

--- tests/server-test/server.spec.js
+++ tests/server-test/server.spec.js
@@ -1,3 +1,5 @@
+/* eslint-env mocha */
+
 const expect = require('chai').expect;
 const app = `${process.env.PROTOCOL}${process.env.HOST}:${process.env.PORT}`;
 const request = require('supertest');
