3204100813ed356f061215adac0dba418270331c
Bill Ramsey <billramsey@gmail.com>
2016-06-24 12:16:44 -0700

    Made changes to scenario to allow for input of ip address and added documentation

--- deployed/worker/scenario.js
+++ deployed/worker/scenario.js
@@ -1,40 +1,71 @@
 const parser = require('./parselang.js');
 const request = require('request');
 /*
- * Questions:
-  1.  Times.
-  3.  Download currentPage and manipulate it? cheerio?
-  5.  Proxy and record?
 
-  Issues:
-  JSON tries to escape characters.
 
-  TODO:
-  1. GWT
-  2. fix .times(N) to work
-  3. follow redirects
+How to use this module.
+
+run: parse and execute the script on the passed server.
+
+Arguments:
+<Host>, <Language String>
+Returns:
+
+The language is very simple right now. See below for descriptions of what you can send.
+scripting language:
+
+login('/path', {"username":"username", "password":"password"})
+Arguments:
+<Path>, <JSON arguments to send the server>
+Description:
+Login log's into a website that uses cookies as authentication.  It stores the cookies
+for each subsequent request.
+
+get('/path')
+Arguments:
+<Path>
+Description:
+performs a get request on the target server.
+
+post('/path')
+Arguments:
+<Path>
+Description:
+performs a post request on the target server.
+
+These commands are chained with a period to create a user scenario.
+
+login('/path', {"username":"username", "password":"password"})
+.get('/')
+.post('/newEntry')
+
+parse:  Send in a string to parse in the scripting language.  It returns an object containing all
+the commands to run.
+Arguments: <Language String>
+
 */
-// login('/login', 'jwt || cookie', [userPassFile], username, password)
-// const scenario = `
-// site('localhost:4568')
-// .login({username: 'bill', password: 'password'})
-// .get('/')
-// .post()
-// .times(3).get('/fileListing')
-// .times(3).values({ "hello": "one"}).post('/newEntry')
-// .times(3).random().select('footerLinks').click()
-// .get('/logout');
-// `;
 
 const parse = (str) => {
   try {
     return parser.parse(str.trim());
   } catch (err) {
-    console.log('error parsing scenario', err.message, 'at line', err.location.start.line,
+    console.log('error parsing', err.message, 'at line', err.location.start.line,
       ', column', err.location.start.column);
     return undefined;
   }
 };
+
+const env = {};
+const execute = ({ action, args }, modifiers) =>
+  new Promise((resolve, reject) => {
+    // console.log('evaluation ', action);
+    try {
+      // console.log('calling ', action, ' with args:', args);
+      env[action].apply(null, [modifiers].concat(args)).then(resolve).catch(reject);
+    } catch (err) {
+      reject(err);
+    }
+  });
 
 const run = (site, command) => {
   const actions = parse(command);
@@ -44,27 +75,22 @@ const run = (site, command) => {
   }
 
   let i = 0;
-  return Promise.resolve({ site }).then(function loop(modifiers) {
+  const startTime = new Date().getTime();
+  const transactionTimes = [];
+  return Promise.resolve({ site, transactionTimes }).then(function loop(modifiers) {
     //  console.log(i);
     if (i < actions.length) {              // The post iteration increment
       return execute(actions[i++], modifiers).then(loop).catch((err) => {
         console.log('fail', err);
         Promise.reject(err);
       });
-    }
-  });
-};
-
-
-const env = {};
-const execute = ({ action, args }, modifiers) => {
-  return new Promise((resolve, reject) => {
-    // console.log('evaluation ', action);
-    try {
-      // console.log('calling ', action, ' with args:', args);
-      env[action].apply(null, [modifiers].concat(args)).then(resolve).catch(reject);
-    } catch (err) {
-      reject(err);
+    } else {
+      const endTime = new Date().getTime();
+      const results = {
+        scenarioTime: endTime - startTime,
+        transactionTimes: modifiers.transactionTimes,
+      };
+      return Promise.resolve(results);
     }
   });
 };
@@ -85,7 +111,7 @@ const timedRequest = (requestInfo, cookies) => {
       res.on('end', () => {
         //  console.log('rinfo:', requestInfo);
         //  console.log('data,', data);
-        console.log(res.statusCode, res.elapsedTime, length);
+        res.scenario_length = length;
         resolve(res);
       });
     });
@@ -118,7 +144,8 @@ env.get = (modifiers, path) => {
     }, modifiers.cookies)
     .then((res) => {
       // console.log(res);
-      resolve();
+      modifiers.transactionTimes.push([path, res.statusCode, res.elapsedTime, res.scenario_length]);
+      resolve(modifiers);
     })
     .catch((err) => {
       console.log('error received during http request.');
@@ -136,7 +163,8 @@ env.post = (modifiers, path) => {
       url: site + path,
     }, modifiers.cookies)
     .then((res) => {
-      resolve(res);
+      modifiers.transactionTimes.push([path, res.statusCode, res.elapsedTime, res.scenario_length]);
+      resolve(modifiers);
     })
     .catch((err) => {
       console.log('error received during http request.');
@@ -145,12 +173,14 @@ env.post = (modifiers, path) => {
   });
 };
 
-env.times = (modifiers, number) => {
-  return new Promise((resolve, reject) => {
+env.times = (modifiers, number) =>
+  new Promise((resolve, reject) => {
+    if (!modifiers) {
+      reject('modifiers does not exist');
+    }
     modifiers.times = number;
     resolve(modifiers);
   });
-};
 
 
 env.login = (modifiers, path, data) => {
@@ -168,6 +198,7 @@ env.login = (modifiers, path, data) => {
     })
     .then((res) => {
       modifiers.cookies = j;
+      modifiers.transactionTimes.push([path, res.statusCode, res.elapsedTime, res.scenario_length]);
       resolve(modifiers);
     })
     .catch((err) => {
@@ -177,6 +208,31 @@ env.login = (modifiers, path, data) => {
   });
 };
 
+/*
+ * Questions:
+  1.  Times.
+  3.  Download currentPage and manipulate it? cheerio?
+  5.  Proxy and record?
+
+  Issues:
+  JSON tries to escape characters.
+
+  TODO:
+  1. GWT
+  2. fix .times(N) to work
+  3. follow redirects
+*/
+// login('/login', 'jwt || cookie', [userPassFile], username, password)
+// const scenario = `
+// site('localhost:4568')
+// .login({username: 'bill', password: 'password'})
+// .get('/')
+// .post()
+// .times(3).get('/fileListing')
+// .times(3).values({ "hello": "one"}).post('/newEntry')
+// .times(3).random().select('footerLinks').click()
+// .get('/logout');
+// `;
 
 module.exports = { timedRequest, run, parse };
 
--- tests/deployed/scenario-run.spec.js
+++ tests/deployed/scenario-run.spec.js
@@ -2,9 +2,10 @@
 const request = require('request');
 const sinon = require('sinon');
 const scenario = require('../../deployed/worker/scenario.js');
+var expect = require('chai').expect;
 
 describe('User Profile', () => {
-  const site = 'http:localhost:3000';
+  const site = 'http://localhost:3000';
 
   before((done) => {
     sinon
@@ -19,8 +20,10 @@ describe('User Profile', () => {
   });
 
   it('can log in using JWT', (done) => {
-    const script = `site(\'http://localhost:3000\').login(\'/login\', {"username":"bill", "password":"password"}).get(\'/links\');`;
-    scenario.run(site, script).then(() => {
+    const script = `login(\'/login\', {"username":"bill", "password":"password"}).get(\'/links\');`;
+    scenario.run(site, script).then((results) => {
+      (results.scenarioTime).should.be.above(0);
+      expect(results.transactionTimes).to.be.instanceof(Array);
       done();
     }).catch(done);
   });
