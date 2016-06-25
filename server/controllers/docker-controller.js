8617c3224f36564c17153cb8ff6b0748c139d965
cshg <csh@christianhaug.com>
2016-06-24 17:35:37 -0700

    Implement createWorker function for new container

--- server/controllers/docker-controller.js
+++ server/controllers/docker-controller.js
@@ -2,27 +2,40 @@
 const fs = require('fs');
 const Docker = require('dockerode');
 
-const docker = new Docker({socketPath: '/var/run/docker.sock'});
-const docker1 = new Docker({
+const dockerConfig = new Docker({socketPath: '/var/run/docker.sock'});
+const dockerConnection = new Docker({
   host: '45.55.183.145',
   port: 2376,
   ca: fs.readFileSync('../certificates/do1/ca.pem'),
   cert: fs.readFileSync('../certificates/do1/cert.pem'),
-  key: fs.readFileSync('../certificates/do1/key.pem')
+  key: fs.readFileSync('../certificates/do1/key.pem'),
 });
 
-const createContainer = (req, res) => {
-  console.log('createContainer called');
-  // implement docker instance creation
-
-  res.status(201).send('container successfully created and running');
+const status = {
+  workerCount: 0
 };
 
-const checkContainer = (req, res) => {
+const createWorker = (req, res) => {
+  status.workerCount++;
+  const workerName = 'worker'.concat(status.workerCount);
+  dockerConnection.createContainer({ Image: 'node-sender', name: workerName }, (err, container) => {
+    container.start((startErr, data) => {
+      if (err) {
+        console.log('error while starting new container', startErr);
+        res.status(500).send('error while starting new container', err);
+      } else {
+        console.log('container started with the following id: ', container.id);
+        res.status(201).send('container successfully created and running');
+      }
+    });
+  });
+};
+
+const checkWorker = (req, res) => {
   console.log('checkContainer called');
   // implement docker container check
 
   res.status(200).send('container checked');
 };
 
-module.exports = { createContainer, checkContainer };
+module.exports = { createWorker, checkWorker };
