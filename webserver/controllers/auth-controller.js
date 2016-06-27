1435034f5e4126f451274bcc4d4c583ce1495fea
Tai Huynh <anhtaih89@gmail.com>
2016-06-27 14:22:29 -0700

    Refactor code to clean up and make more concise

--- webserver/controllers/auth-controller.js
+++ webserver/controllers/auth-controller.js
@@ -5,59 +5,38 @@ const utils = require('../lib/utils');
 const sendJSON = utils.sendJSON;
 
 const signup = (req, res) => {
-  const userObj = {
-    name: req.body.name,
-    email: req.body.email,
-  };
-
-  if (!userObj.name || !userObj.email || !req.body.password) {
-    sendJSON(res, 400, {
-      message: 'All fields required',
-    });
-    return;
+  if (!req.body.name || !req.body.email || !req.body.password) {
+    return sendJSON(res, 400, { message: 'All fields required' });
   }
 
-  User.where('email', userObj.email).fetch()
+  User.where('email', req.body.email).fetch()
     .then(existingUser => {
       if (!existingUser) {
-        const newUser = new User(userObj);
+        const newUser = new User({ name: req.body.name, email: req.body.email });
         newUser.setPassword(req.body.password);
         newUser.save()
         .then(() => {
-          sendJSON(res, 200, {
-            id_token: newUser.generateJwt(),
-          });
+          sendJSON(res, 200, { id_token: newUser.generateJwt() });
         });
 
         return newUser;
       }
     })
-    .catch(err => {
-      sendJSON(res, 404, err);
-    });
+    .catch(err => sendJSON(res, 404, err));
 };
 
 const login = (req, res, next) => {
   if (!req.body.email || !req.body.password) {
-    sendJSON(res, 400, {
-      message: 'All fields required',
-    });
-    return;
+    return sendJSON(res, 400, { message: 'All fields required' });
   }
 
   passport.authenticate('local', (err, user, info) => {
-    let token;
-
     if (err) {
-      sendJSON(res, 404, err);
-      return;
+      return sendJSON(res, 404, err);
     }
 
     if (user) {
-      token = user.generateJwt();
-      sendJSON(res, 200, {
-        id_token: token,
-      });
+      sendJSON(res, 200, { id_token: user.generateJwt() });
     } else {
       sendJSON(res, 401, info);
     }
