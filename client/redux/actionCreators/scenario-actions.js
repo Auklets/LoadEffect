edb59cf7d4bec132c33ce809329466542b4654fa
Tai Huynh <anhtaih89@gmail.com>
2016-06-27 12:31:03 -0700

    Remove setting user id to local storage due to it already being included in jwt sent back and forth between client and web server

--- client/redux/actionCreators/scenario-actions.js
+++ client/redux/actionCreators/scenario-actions.js
@@ -1,10 +1,14 @@
 export const GET_SCENARIOS = 'GET_SCENARIOS';
 
 export const getData = () => {
+  const token = localStorage.getItem('id_token');
+
   const config = {
     method: 'GET',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-               id_user: localStorage.getItem('id') },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Bearer ${token}`,
+    },
   };
 
   return dispatch =>
