0d0f95e324cac117620ba537d0e83c2ea250d1a5
Felix <felix2feng@gmail.com>
2016-06-29 21:24:35 -0700

    Merge conflict

--- webserver/controllers/results-contoller.js
+++ webserver/controllers/results-contoller.js
@@ -2,4 +2,17 @@ const Spawn = require('../models/SpawnsModel');
 const request = require('request');
 const utils = require('../lib/utils');
 
-const 
+
+/* EXTRA CREDIT IDEAS
+  // Get data from spawns database
+    // Get data from DB where timestamp > the last global Variable
+      // And where id scenario equals the one we want
+    // set lastpull to the latest timestamp of the data received
+    // respond with data from database
+*/
+
+const getData = (req, res) => {
+}
+
+
+module.exports = { getData };
