1b1b78c6d467f3510631e309e60d2ef90b69d4a7
Felix <felix2feng@gmail.com>
2016-07-01 19:58:08 -0700

    Built in sockets and tested

--- client/redux/actionCreators/liveResults-actions.js
+++ client/redux/actionCreators/liveResults-actions.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import io from 'socket.io-client';
 
 export const UPDATE_LINE_CHART = 'UPDATE_LINE_CHART';
@@ -6,6 +5,8 @@ export const UPDATE_CURRENT_ACTION = 'UPDATE_CURRENT_ACTION';
 
 const socket = io();
 
+// REMOVE COUNTER FOR PRODUCTION
+let tempCounter = 0;
 // Update line chart data
 export const updateLineChartData = (jobCount, scenarioID) => {
   console.log('We have called updateLineChartData');
@@ -33,36 +34,16 @@ export const updateLineChartData = (jobCount, scenarioID) => {
       });
       console.log('This is job count', jobCount);
       console.log('This is data length', data.spawn.labels.length);
-      if (data.spawn.labels.length < jobCount) {
+      // REMOVE COUNTER FOR PRODUCTION
+      if (data.spawn.labels.length < jobCount && tempCounter < 100) {
         console.log('We are recursively calling');
-        dispatch(updateLineChartData(jobCount, scenarioID));
+        // REMOVE TEST SCENARIO FOR PRODUCTION
+        const testScenarioID = 15;
+        dispatch(updateLineChartData(jobCount, testScenarioID));
+        // REMOVE COUNTER FOR PRODUCTION
+        tempCounter++;
+        console.log('tempCounter count is', tempCounter);
       }
     });
   };
-    // HTTP AXIOS REQUEST
-    // axios.post(serverEndPoint, { currentScenarioID: scenarioID })
-    //   .then(res => {
-    //     console.log('This is the response from the server', res);
-    //     const spawnData = res.data.spawn;
-    //     const actionData = res.data.action;
-    //     dispatch({
-    //       type: UPDATE_LINE_CHART,
-    //       labels: spawnData.labels,
-    //       series: spawnData.series,
-    //     });
-    //     dispatch({
-    //       type: UPDATE_CURRENT_ACTION,
-    //       index: actionData.index,
-    //       httpVerb: actionData.httpVerb,
-    //       statusCode: actionData.statusCode,
-    //       elapsedTime: actionData.elapsedTime,
-    //     });
-    //     console.log('This is job count', jobCount);
-    //     console.log('This is data length', res.data.spawn.labels.length);
-    //     if (res.data.labels.length < jobCount) {
-    //       console.log('We are recursively calling');
-    //       dispatch(updateLineChartData(jobCount, scenarioID));
-    //     }
-    //   }
-    // ).catch(err => console.log('Error: ', err));
 };
