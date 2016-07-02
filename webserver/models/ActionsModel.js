eb8852f85ec6f60465e7a836566c535e33bbb1c3
Tai Huynh <anhtaih89@gmail.com>
2016-07-01 21:27:08 -0700

    Update models according to spec changes, add website validation using dns module and hash generator, remove unused functions

--- webserver/models/ActionsModel.js
+++ webserver/models/ActionsModel.js
@@ -5,11 +5,11 @@ db.knex.schema.hasTable('actions').then(exists => {
   if (!exists) {
     db.knex.schema.createTable('actions', action => {
       action.increments('id').primary();
-      action.string('actionName', 255).unique();
+      action.string('actionTaken', 255);
+      action.string('Path', 255);
       action.integer('statusCode', 255);
       action.integer('elapsedTime', 255);
       action.integer('id_scenario', 255);
-      action.integer('dataSizeInBytes', 255);
       action.string('httpVerb', 255);
       action.timestamps();
     }).then(table => {
