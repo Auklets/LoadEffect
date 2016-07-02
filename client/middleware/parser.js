13384d790d83259a745b5af68a64bf3194e99bb9
Bill Ramsey <billramsey@gmail.com>
2016-07-01 19:48:22 -0700

    Initial Commit of client side script validating

---
+++ client/middleware/parser.js
@@ -0,0 +1,70 @@
+//  parser.js
+//
+const peg = require('pegjs');
+
+const parserArguments = `
+start 
+  = nl* first:line rest:(nl+ data:line { return data; })* nl* { 
+    rest.unshift(first); return rest;
+  }
+    
+line = space first:command tail:(" " argument)* {
+  var params = [];
+  for (var i = 0; i < tail.length; i++) {
+    params.push(tail[i][1]);
+  }
+  return {type:'CallExpression', operator:first, params: params}
+}
+//
+argument "argument"
+  = func / JSON / number / string
+command "command"
+= head:[^\\t\\n\\r )]* {
+  return {type:"word", name:head.join("")};
+}
+func = "(" space space first:command tail:(" " argument)* space ")" {
+  var params = [];
+  for (var i = 0; i < tail.length; i++) {
+    params.push(tail[i][1]);
+  }
+  return {type:'DelayExpression', operator:first, params: params}
+
+ }
+string "string" = s:[^\\t\\n\\r() ]+ {
+  return {type:"value", value:s.join("")};
+}
+number "number" = num:[0-9]+ {
+return {type: "value", value:parseInt(num.join(""), 10)};
+}
+comment "comment"
+ = "#"+ [^\\n\\r]* { return ''; }
+JSON = head:("{" $[^}]* "}") { 
+  return {type:"value", value:head.join("")};
+}
+space "space" 
+  = [ ]*
+_ "whitespace"
+  = [ \\t\\n\\r]*
+nl "newline"
+ = [\\n\\r]
+`;
+
+console.log(peg);
+
+const parser = peg.buildParser(parserArguments.trim());
+
+const parseTest = (str) => {
+  try {
+    parser.parse(str.trim());
+    return { success: true };
+  } catch (err) {
+    return {
+      success: false,
+      error: err.message,
+      line: err.location.start.line,
+      column: err.location.start.column,
+    };
+  }
+};
+
+module.exports = { parseTest };
