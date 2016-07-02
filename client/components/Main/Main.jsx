64b62c4d15f9f567a8897fee4e09d9ab1497c9d1
Tai Huynh <anhtaih89@gmail.com>
2016-07-01 21:29:34 -0700

    Change main component according to schema redesign and add verification views and modal to check verification

--- client/components/Main/Main.jsx
+++ client/components/Main/Main.jsx
@@ -1,7 +1,10 @@
 import React, { PropTypes } from 'react';
-import { Table, Jumbotron } from 'react-bootstrap';
+import { Button, Table, Jumbotron, Well } from 'react-bootstrap';
+import VerifySiteContainer from '../VerifySite/VerifySiteContainer.jsx';
 
 const Main = (props) => {
+  const { showVerifyModal, allScenarios } = props;
+
   const tableInstance = (
     <Table responsive striped hover bordered>
       <thead>
@@ -10,25 +13,31 @@ const Main = (props) => {
           <th>Scenario Name</th>
           <th>Spawn Count</th>
           <th># of Workers</th>
-          <th>Average Response Time</th>
           <th>Average Action Time</th>
           <th>Target URL</th>
+          <th>Website Validation</th>
         </tr>
       </thead>
       <tbody>
-        {props.allScenarios.map((item, i) => {
+        {allScenarios.map((item, i) => {
           return (
             <tr key={i}>
               <td>{i + 1}</td>
               <td>{item.scenarioName}</td>
               <td>{item.spawnsCount}</td>
               <td>{item.workers}</td>
-              <td>{item.averageResponseTime}</td>
               <td>{item.averageActionTime}</td>
               <td>{item.targetURL}</td>
+              <td>
+                {!!item.isVerifiedOwner ?
+                  (<span style={{ color: 'green' }}>Verified</span>) :
+                  (<span style={{ color: 'red' }}>Unverified</span>)
+                }
+              </td>
             </tr>
             );
         })}
+        <VerifySiteContainer allScenarios={allScenarios} />
       </tbody>
     </Table>
   );
@@ -40,14 +49,20 @@ const Main = (props) => {
         This is going to be the main page, the protected route that the user should
         be able to get to after successfully logging in.
         See below for summary of scenarios that have been ran in the past.
+
       </p>
-      {tableInstance}
+      <Button onClick={showVerifyModal} bsSize="xlarge" bsStyle="danger">Show Website Token</Button>
+      <hr />
+      <Well>
+        {tableInstance}
+      </Well>
     </Jumbotron>
   );
 };
 
 Main.propTypes = {
   allScenarios: PropTypes.array,
+  showVerifyModal: PropTypes.func,
 };
 
 export default Main;
---
+++ client/components/VerifySite/VerifySiteContainer.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { closeVerifyModal, openVerifyModal } from '../../redux/actionCreators/modal-actions';
+import { getScenarios, checkForValidUrl } from '../../redux/actionCreators/scenario-actions';
+
+import VerifySiteModal from './VerifySiteModal.jsx';
+
+export const VerifySiteContainer = props => (
+  <div>
+    <VerifySiteModal {...props} />
+  </div>
+);
+
+const mapStateToProps = state => {
+  const { auth, modal } = state;
+  const { errorMessage, siteToken } = auth;
+  const { isVerifyModalOpen } = modal;
+
+  return {
+    isVerifyModalOpen,
+    siteToken,
+    errorMessage,
+    state,
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  hideVerifyModal() {
+    dispatch(closeVerifyModal());
+  },
+
+  showVerifyModal() {
+    dispatch(openVerifyModal());
+  },
+
+  getScenarioData() {
+    dispatch(getScenarios());
+  },
+
+  validateUrl(url, scenarioID) {
+    dispatch(checkForValidUrl(url, scenarioID));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(VerifySiteContainer);
+
