29c71c3087df62cc43fd7b342148abd799d365f2
cshg <csh@christianhaug.com>
2016-07-04 15:49:17 -0700

    Cleanup dockerController and dockerConnection

--- webserver/config/docker-config.js
+++ webserver/config/docker-config.js
@@ -4,6 +4,7 @@ const Docker = require('dockerode');
 const dockerModemConfig = new Docker({ socketPath: '/var/run/docker.sock' });
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
+  console.log('create dockerConnection');
   const dockerConnection = new Docker({
     host: process.env.DOCKER_HOST,
     port: process.env.DOCKER_PORT,
