28057eee62c9d4a7fc62758cc5bb4868cfc27315
Felix <felix2feng@gmail.com>
2016-07-06 20:15:02 -0700

    Set up for production version

--- client/components/Results/Results.jsx
+++ client/components/Results/Results.jsx
@@ -12,11 +12,11 @@ class Results extends Component {
     super(props);
 
     const totalSpawns = props.scenario.currentSpawnsCount;
-    // const currentScenarioID = props.scenario.currentScenarioID;
-    // this.props.updateLineChartData(totalSpawns, currentScenarioID);
+    const currentScenarioID = props.scenario.currentScenarioID;
+    this.props.updateLineChartData(totalSpawns, currentScenarioID);
 
-    const testScenarioID = 15;
-    this.props.updateLineChartData(totalSpawns, testScenarioID);
+    // const testScenarioID = 15;
+    // this.props.updateLineChartData(totalSpawns, testScenarioID);
   }
 
   render() {
