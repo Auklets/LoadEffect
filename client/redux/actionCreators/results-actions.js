c79b51401913e479181cac9052ac666c533177a2
Felix <felix2feng@gmail.com>
2016-07-07 11:33:17 -0700

    Refactored code so that there are fewer dispatches

--- client/redux/actionCreators/results-actions.js
+++ client/redux/actionCreators/results-actions.js
@@ -2,9 +2,7 @@ import io from 'socket.io-client';
 import { calculateAverage, percentCompletion, errorCounter } from '../../lib/results-helpers';
 import { storeRecentScenarioInfo } from './scenario-actions';
 
-export const UPDATE_LINE_CHART = 'UPDATE_LINE_CHART';
-export const UPDATE_CURRENT_ACTION = 'UPDATE_CURRENT_ACTION';
-export const UPDATE_COMPUTED = 'UPDATE_COMPUTED';
+export const UPDATE_ALL_CHART = 'UPDATE_ALL_CHART';
 
 const token = localStorage.getItem('id_token');
 const socket = io({
@@ -12,37 +10,35 @@ const socket = io({
 });
 
 let maxRecurse = 0;
+let emitCount = 0;
 /* ******* Update Line Chart Data Actions ******* */
 
-export const updateLineChartAction = spawnData => ({
-  type: UPDATE_LINE_CHART,
-  spawnLabel: spawnData.spawnLabel,
-  elapsedTimeSpawn: spawnData.elapsedTimeSpawn,
-});
-
-export const updateCurrentAction = actionData => ({
-  type: UPDATE_CURRENT_ACTION,
+export const updateAllChart = (spawnData, actionData, averageElapsedTime, numberActions, currentSpawns, percentComplete, numberErrors) => ({
+  type: UPDATE_ALL_CHART,
+  averageElapsedTime,
+  numberErrors,
+  numberActions,
+  currentSpawns,
+  percentComplete,
   index: actionData.index,
   httpVerb: actionData.httpVerb,
   statusCode: actionData.statusCode,
   elapsedTimeAction: actionData.elapsedTimeAction,
   actionTaken: actionData.actionTaken,
   path: actionData.path,
-});
-
-export const updateComputedData = (averageElapsedTime, numberActions, currentSpawns, percentComplete, numberErrors) => ({
-  type: UPDATE_COMPUTED,
-  averageElapsedTime,
-  numberErrors,
-  numberActions,
-  currentSpawns,
-  percentComplete,
+  spawnLabel: spawnData.spawnLabel,
+  elapsedTimeSpawn: spawnData.elapsedTimeSpawn,
 });
 
 export const updateLineChartData = (jobCount, scenarioID) =>
   dispatch => {
+    if (emitCount > 10) {
+      return;
+    }
+
     socket.emit('getResultsData', { currentScenarioID: scenarioID });
-    console.log('Making an emit to web server');
+    emitCount++;
+    console.log('Making an emit to web server', emitCount);
     socket.on('receiveResultsData', (data) => {
       console.log('Got data from sockets', data);
       socket.removeAllListeners('receiveResultsData');
@@ -51,9 +47,6 @@ export const updateLineChartData = (jobCount, scenarioID) =>
       const { elapsedTimeSpawn, spawnLabel } = spawn;
       const { httpVerb, statusCode } = action;
 
-      dispatch(storeRecentScenarioInfo(scenario));
-      dispatch(updateLineChartAction(spawn));
-      dispatch(updateCurrentAction(action));
 
       const { averageElapsedTime, numberActions } = scenario;
       const calculated = {
@@ -63,16 +56,21 @@ export const updateLineChartData = (jobCount, scenarioID) =>
         percentComplete: percentCompletion(jobCount, spawnLabel.length),
         numberErrors: errorCounter(statusCode),
       };
-      dispatch(updateComputedData(
+      console.log('receive statusCode from socket', statusCode);
+      dispatch(updateAllChart(
+        spawn,
+        action,
         calculated.averageElapsedTime,
         calculated.numberActions,
         calculated.currentSpawns,
         calculated.percentComplete,
-        calculated.numberErrors,
+        calculated.numberErrors
       ));
+      dispatch(storeRecentScenarioInfo(scenario));
 
       if (!scenario.completion) {
-        if (elapsedTimeSpawn.length < jobCount && maxRecurse < 500) {
+        if (elapsedTimeSpawn.length < jobCount && maxRecurse < 3) {
+          console.log('maxRecurse', maxRecurse);
           maxRecurse++;
           dispatch(updateLineChartData(jobCount, scenarioID, calculated));
         } else {
--- client/redux/reducers/chart-reducer.js
+++ client/redux/reducers/chart-reducer.js
@@ -1,4 +1,4 @@
-import { UPDATE_LINE_CHART, UPDATE_CURRENT_ACTION, UPDATE_COMPUTED } from '../actionCreators/results-actions';
+import { UPDATE_ALL_CHART } from '../actionCreators/results-actions';
 
 const initialStateChart = {
   spawnLabel: [1, 2, 3, 4, 5, 6, 7, 8],
@@ -18,29 +18,21 @@ const initialStateChart = {
 
 export const chartReducer = (state = initialStateChart, action) => {
   switch (action.type) {
-    case UPDATE_LINE_CHART:
-      return Object.assign({}, state, {
-        spawnLabel: action.spawnLabel,
-        elapsedTimeSpawn: action.elapsedTimeSpawn,
-      });
-
-    case UPDATE_CURRENT_ACTION:
-      return Object.assign({}, state, {
-        index: action.index,
-        httpVerb: action.httpVerb,
-        statusCode: action.statusCode,
-        elapsedTimeAction: action.elapsedTimeAction,
-        actionTaken: action.actionTaken,
-        path: action.path,
-      });
-
-    case UPDATE_COMPUTED:
+    case UPDATE_ALL_CHART:
       return Object.assign({}, state, {
         averageElapsedTime: action.averageElapsedTime,
         numberErrors: action.numberErrors,
         numberActions: action.numberActions,
         currentSpawns: action.currentSpawns,
         percentComplete: action.percentComplete,
+        index: action.index,
+        httpVerb: action.httpVerb,
+        statusCode: action.statusCode,
+        elapsedTimeAction: action.elapsedTimeAction,
+        actionTaken: action.actionTaken,
+        path: action.path,
+        spawnLabel: action.spawnLabel,
+        elapsedTimeSpawn: action.elapsedTimeSpawn,
       });
 
     default:
