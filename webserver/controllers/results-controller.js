d31c316ef73d0b82add1248598920b2e7ca639e1
Felix <felix2feng@gmail.com>
2016-07-08 13:30:16 -0700

    Cleaned up code and added average elapsed time

--- webserver/controllers/results-controller.js
+++ webserver/controllers/results-controller.js
@@ -2,17 +2,8 @@ const { getFromSpawn, getFromActions, getFromScenario } = require('../lib/helper
 const Scenario = require('../models/ScenariosModel');
 const Promise = require('bluebird');
 
-/* EXTRA CREDIT IDEAS
-  // Get database where scenario ID and id is greater than the greatest id
-    // Get data from DB where timestamp > the last global Variable
-      // And where id scenario equals the one we want
-    // set lastpull to the latest timestamp of the data received
-    // respond with data from database
-*/
-
 const getResultsDataHandler = (socket) =>
   (req) => {
-    // console.log('Received request in results controller get data!', req);
     const scenarioID = req.currentScenarioID;
 
     Promise.all([getFromActions(scenarioID), getFromSpawn(scenarioID), getFromScenario(scenarioID)])
@@ -28,7 +19,6 @@ const getResultsDataHandler = (socket) =>
   };
 
 const saveCompletedData = (data) => {
-  // console.log('Got data from completed data', data);
   const calculated = data.calculated;
   Scenario.where('id', data.scenarioID)
     .fetch()
